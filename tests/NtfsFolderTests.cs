a684a07f2ce668ed85fd8fe6a270e7ede45ebb47
itsWindows11 <itswin11@outlook.com>
2026-03-08 16:46:00 +0200

    Moved tests
    Made this change approx. 2 years ago so IDK why I did this 🫠

---
+++ tests/NtfsFolderTests.cs
@@ -0,0 +1,28 @@
+﻿using OwlCore.Storage.CommonTests;
+using System.IO.Filesystem.Ntfs;
+
+namespace OwlCore.Storage.Ntfs.Tests;
+
+[TestClass]
+public class NtfsFolderTests : CommonIFolderTests
+{
+    private NtfsReader? reader;
+
+    public override Task<IFolder> CreateFolderAsync()
+    {
+        reader ??= new NtfsReader(new DriveInfo("C:\\"), RetrieveMode.Minimal);
+
+        // We cannot actually create a folder, so we just return any folder that exists.
+        // We use the user's temp folder here.
+        return Task.FromResult<IFolder>(new NtfsFolder(reader, Path.GetTempPath()));
+    }
+
+    public override Task<IFolder> CreateFolderWithItems(int fileCount, int folderCount)
+    {
+        reader ??= new NtfsReader(new DriveInfo("C:\\"), RetrieveMode.Minimal);
+
+        // We cannot actually create a folder, so we just return any folder that exists.
+        // We use the user's temp folder here.
+        return Task.FromResult<IFolder>(new NtfsFolder(reader, Path.GetTempPath()));
+    }
+}
